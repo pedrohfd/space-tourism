0e2ddc32a8cee44ed686ab9f2e633ab98b1127fe
Pedro Henrique <pedrohenriquefrancadedeus@gmail.com>
2023-07-04 11:40:49 -0300

    feat: configuring destination route

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,13 +1,14 @@
 import { Routes, Route } from 'react-router-dom'
 import { Home } from '../pages/Home'
 import { DefaultLayout } from '../layouts/DefaultLayout'
+import { Destination } from '../pages/Destination'
 
 export const Router = () => {
   return (
     <Routes>
       <Route path='/' element={<DefaultLayout />}>
         <Route path='/' element={<Home />} />
-        {/* <Route /> */}
+        <Route path='/destination' element={<Destination />} />
         {/* <Route /> */}
         {/* <Route /> */}
       </Route>

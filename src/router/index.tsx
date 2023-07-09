2b85e001cbc6c5f1a38d2ad91f0bb8ded184c2b1
Pedro Henrique <pedrohenriquefrancadedeus@gmail.com>
2023-07-09 12:09:52 -0300

    feat: add technology route

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -3,6 +3,7 @@ import { Home } from '../pages/Home'
 import { DefaultLayout } from '../layouts/DefaultLayout'
 import { Destination } from '../pages/Destination'
 import { Crew } from '../pages/Crew'
+import { Technology } from '../pages/Technology'
 
 export const Router = () => {
   return (
@@ -11,7 +12,7 @@ export const Router = () => {
         <Route path='/' element={<Home />} />
         <Route path='/destination' element={<Destination />} />
         <Route path='/crew' element={<Crew />} />
-        {/* <Route /> */}
+        <Route path='/technology' element={<Technology />} />
       </Route>
     </Routes>
   )

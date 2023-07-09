28bdd5a4cacc4db617447ed9e31c3b0894ddcf74
Pedro Henrique <pedrohenriquefrancadedeus@gmail.com>
2023-07-09 11:59:58 -0300

    feat: create crew page

---
+++ src/pages/Crew/index.tsx
@@ -0,0 +1,92 @@
+import { useState } from 'react'
+import {
+  CrewContainer,
+  CrewContent,
+  Informative,
+  LeftContent,
+  RightContent,
+  Role,
+  Name,
+  Body,
+  CrewMember,
+  CrewButton,
+  CrewButtonArea,
+} from './styles'
+import ansari from '../../assets/crew/image-anousheh-ansari.webp'
+import douglas from '../../assets/crew/image-douglas-hurley.webp'
+import mark from '../../assets/crew/image-mark-shuttleworth.webp'
+import victor from '../../assets/crew/image-victor-glover.webp'
+import data from '../../data.json'
+
+export const Crew = () => {
+  const [crew, setCrew] = useState(data.crew[0])
+
+  const [image, setImage] = useState(douglas)
+
+  const handleCrew = (crew: string) => {
+    switch (crew) {
+      case 'douglas':
+        setCrew(data.crew[0])
+        setImage(douglas)
+        break
+
+      case 'mark':
+        setCrew(data.crew[1])
+        setImage(mark)
+        break
+
+      case 'victor':
+        setCrew(data.crew[2])
+        setImage(victor)
+        break
+
+      case 'ansari':
+        setCrew(data.crew[3])
+        setImage(ansari)
+        break
+    }
+  }
+
+  return (
+    <CrewContainer>
+      <CrewContent>
+        <LeftContent>
+          <Informative>
+            <strong>02</strong> MEET YOUR CREW
+          </Informative>
+
+          <Role>{crew.role}</Role>
+
+          <Name>{crew.name}</Name>
+
+          <Body name={crew.name}>{crew.bio}</Body>
+
+          <CrewButtonArea>
+            <CrewButton
+              isActive={crew.name === 'Douglas Hurley'}
+              onClick={() => handleCrew('douglas')}
+            />
+
+            <CrewButton
+              isActive={crew.name === 'Mark Shuttleworth'}
+              onClick={() => handleCrew('mark')}
+            />
+
+            <CrewButton
+              isActive={crew.name === 'Victor Glover'}
+              onClick={() => handleCrew('victor')}
+            />
+
+            <CrewButton
+              isActive={crew.name === 'Anousheh Ansari'}
+              onClick={() => handleCrew('ansari')}
+            />
+          </CrewButtonArea>
+        </LeftContent>
+        <RightContent name={crew.name}>
+          <CrewMember src={image} />
+        </RightContent>
+      </CrewContent>
+    </CrewContainer>
+  )
+}
---
+++ src/pages/Crew/styles.ts
@@ -0,0 +1,126 @@
+import styled from 'styled-components'
+
+import bgDesktop from '../../assets/crew/background-crew-desktop.jpg'
+import bgTablet from '../../assets/crew/background-crew-tablet.jpg'
+import bgMobile from '../../assets/crew/background-crew-mobile.jpg'
+import { colors } from '../../styles/colors'
+
+interface CrewProps {
+  name: string
+}
+
+export const CrewContainer = styled.div`
+  height: 100vh;
+
+  @media (min-width: 1200px) {
+    background: url(${bgDesktop}) no-repeat center center/cover;
+    /* padding: 16.3rem 10rem 10.5rem 10rem; */
+  }
+
+  @media (min-width: 400px) and (max-width: 1199px) {
+    background: url(${bgTablet}) no-repeat center center/cover;
+  }
+
+  @media (max-width: 399px) {
+    background: url(${bgMobile}) no-repeat center center/cover;
+  }
+`
+
+export const CrewContent = styled.div`
+  @media (min-width: 1200px) {
+    display: flex;
+    justify-content: space-between;
+  }
+`
+
+export const LeftContent = styled.div`
+  @media (min-width: 1200px) {
+    padding: 16.3rem 0 10.5rem 10rem;
+  }
+`
+
+export const RightContent = styled.div<CrewProps>`
+  @media (min-width: 1200px) {
+    padding: ${(props) =>
+      props.name === 'Douglas Hurley' ? '17rem 10rem 0 0' : '22rem 10rem 0 0'};
+  }
+`
+
+export const Informative = styled.h5`
+  @media (min-width: 1200px) {
+    color: ${colors.white};
+    font-size: 1.75rem;
+
+    letter-spacing: 0.29rem;
+
+    margin-bottom: 9rem;
+
+    strong {
+      opacity: 0.25;
+      font-weight: 700;
+      margin-right: 1.5rem;
+    }
+  }
+`
+
+export const Name = styled.h1`
+  @media (min-width: 1200px) {
+    color: ${colors.white};
+    font-size: 3.5rem;
+    text-transform: uppercase;
+  }
+`
+
+export const Role = styled.h2`
+  @media (min-width: 1200px) {
+    color: ${colors.white};
+    font-size: 2rem;
+    text-transform: uppercase;
+    opacity: 0.5;
+    margin-bottom: 1rem;
+  }
+`
+
+export const Body = styled.p<CrewProps>`
+  @media (min-width: 1200px) {
+    width: ${(props) =>
+      props.name === 'Victor Glover' ? '35rem' : '27.75rem'};
+    color: ${colors.blue};
+    font-size: 1.125rem;
+    line-height: 2rem;
+    margin-top: 1.5rem;
+  }
+`
+
+export const CrewButtonArea = styled.div`
+  @media (min-width: 1200px) {
+    margin-top: 7.5rem;
+    display: flex;
+    justify-content: space-between;
+    width: 9rem;
+  }
+`
+
+interface CrewButtonProps {
+  isActive: boolean
+}
+
+export const CrewButton = styled.button<CrewButtonProps>`
+  @media (min-width: 1200px) {
+    height: 1rem;
+    width: 1rem;
+    border-radius: 50%;
+    border: 0;
+    background: ${colors.white};
+    opacity: ${(props) => (props.isActive ? '1' : '0.17')};
+    cursor: pointer;
+
+    transition: opacity 0.2s;
+
+    &:hover {
+      opacity: ${(props) => (props.isActive ? '1' : '0.5')};
+    }
+  }
+`
+
+export const CrewMember = styled.img``

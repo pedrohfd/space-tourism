062689cc0a5cebd471be277ff1f8d1f6809a158f
Pedro Henrique <pedrohenriquefrancadedeus@gmail.com>
2023-07-12 10:43:56 -0300

    feat: add tablet responsiveness to technology page

--- src/pages/Technology/index.tsx
+++ src/pages/Technology/index.tsx
@@ -14,29 +14,36 @@ import {
 import data from '../../data.json'
 import { useState } from 'react'
 import vehicle from '../../assets/technology/image-launch-vehicle-portrait.jpg'
+import vehicleLandscape from '../../assets/technology/image-launch-vehicle-landscape.jpg'
 import capsule from '../../assets/technology/image-space-capsule-portrait.jpg'
+import capsuleLandscape from '../../assets/technology/image-space-capsule-landscape.jpg'
 import spaceport from '../../assets/technology/image-spaceport-portrait.jpg'
+import spaceportLandscape from '../../assets/technology/image-spaceport-landscape.jpg'
 
 export const Technology = () => {
   const [technology, setTechnology] = useState(data.technology[0])
 
   const [image, setImage] = useState(vehicle)
+  const [imageLandscape, setImageLandscape] = useState(vehicleLandscape)
 
   const handleTechnology = (technology: string) => {
     switch (technology) {
       case 'vehicle':
         setTechnology(data.technology[0])
         setImage(vehicle)
+        setImageLandscape(vehicleLandscape)
         break
 
       case 'spaceport':
         setTechnology(data.technology[1])
         setImage(spaceport)
+        setImageLandscape(spaceportLandscape)
         break
 
       case 'capsule':
         setTechnology(data.technology[2])
         setImage(capsule)
+        setImageLandscape(capsuleLandscape)
         break
     }
   }
@@ -50,6 +57,12 @@ export const Technology = () => {
           </Informative>
 
           <div className='flex'>
+            <TechnologyImage
+              className='landscape'
+              src={imageLandscape}
+              alt={technology.name}
+            />
+
             <TechnologyButtonArea>
               <TechnologyButton
                 onClick={() => handleTechnology('vehicle')}
@@ -82,7 +95,11 @@ export const Technology = () => {
         </LeftContent>
 
         <RightContent>
-          <TechnologyImage src={image} alt={technology.name} />
+          <TechnologyImage
+            className='portrait'
+            src={image}
+            alt={technology.name}
+          />
         </RightContent>
       </TechnologyContent>
     </TechnologyContainer>
--- src/pages/Technology/styles.ts
+++ src/pages/Technology/styles.ts
@@ -14,6 +14,7 @@ export const TechnologyContainer = styled.div`
 
   @media (min-width: 400px) and (max-width: 1199px) {
     background: url(${bgTablet}) no-repeat center center/cover;
+    padding-top: 8.5rem;
   }
 
   @media (max-width: 399px) {
@@ -22,10 +23,10 @@ export const TechnologyContainer = styled.div`
 `
 
 export const TechnologyContent = styled.div`
+  display: flex;
+  justify-content: space-between;
   @media (min-width: 1200px) {
-    display: flex;
     padding: 16.3rem 0rem 10.5rem 10rem;
-    justify-content: space-between;
 
     div.flex {
       display: flex;
@@ -33,11 +34,27 @@ export const TechnologyContent = styled.div`
       margin-top: 8.5rem;
     }
   }
+
+  @media (min-width: 400px) and (max-width: 1199px) {
+    flex-direction: column;
+  }
 `
 
-export const LeftContent = styled.div``
+export const LeftContent = styled.div`
+  @media (min-width: 400px) and (max-width: 1199px) {
+    text-align: center;
 
-export const RightContent = styled.div``
+    h5 {
+      text-align: left;
+    }
+  }
+`
+
+export const RightContent = styled.div`
+  @media (min-width: 400px) and (max-width: 1199px) {
+    text-align: center;
+  }
+`
 
 export const Informative = styled.h5`
   @media (min-width: 1200px) {
@@ -52,6 +69,21 @@ export const Informative = styled.h5`
       margin-right: 1.5rem;
     }
   }
+
+  @media (min-width: 400px) and (max-width: 1199px) {
+    color: ${colors.white};
+    font-size: 1.25rem;
+    letter-spacing: 0.21rem;
+
+    margin-bottom: 2.5rem;
+    margin-left: 2.5rem;
+
+    strong {
+      opacity: 0.25;
+      font-weight: 700;
+      margin-right: 1.1rem;
+    }
+  }
 `
 
 export const TechnologyButtonArea = styled.div`
@@ -59,6 +91,10 @@ export const TechnologyButtonArea = styled.div`
     display: flex;
     flex-direction: column;
     margin-right: 5rem;
+  }
+
+  @media (min-width: 400px) and (max-width: 1199px) {
+    margin: 2.75rem 0;
   }
 `
 
@@ -87,6 +123,23 @@ export const TechnologyButton = styled.button<TechnologyButtonProps>`
       margin-top: 2rem;
     }
   }
+
+  @media (min-width: 400px) and (max-width: 1199px) {
+    background: ${(props) => (props.isActive ? colors.white : 'transparent')};
+    border: 1px solid ${colors.white}40;
+    border-radius: 50%;
+    width: 5rem;
+    height: 5rem;
+    color: ${(props) => (props.isActive ? colors.black : colors.white)};
+    font-family: 'Bellefair', serif;
+    font-size: 2rem;
+
+    transition: all 0.2s;
+
+    & + button {
+      margin-left: 1rem;
+    }
+  }
 `
 
 export const Title = styled.h1`
@@ -97,12 +150,26 @@ export const Title = styled.h1`
 
     margin-top: 0.69rem;
   }
+
+  @media (min-width: 400px) and (max-width: 1199px) {
+    color: ${colors.white};
+    font-size: 2.5rem;
+    text-transform: uppercase;
+    margin: 1rem 0;
+  }
 `
 
 export const Subtitle = styled.h5`
   @media (min-width: 1200px) {
     color: ${colors.blue};
     font-size: 1.25rem;
+    letter-spacing: 0.16875rem;
+  }
+
+  @media (min-width: 400px) and (max-width: 1199px) {
+    display: inline-block;
+    color: ${colors.blue};
+    font-size: 1rem;
     letter-spacing: 0.16875rem;
   }
 `
@@ -116,6 +183,29 @@ export const Description = styled.p`
 
     margin-top: 1rem;
   }
+
+  @media (min-width: 400px) and (max-width: 1199px) {
+    display: inline-block;
+    width: 35.8rem;
+    color: ${colors.blue};
+    line-height: 1.75rem;
+  }
 `
 
-export const TechnologyImage = styled.img``
+export const TechnologyImage = styled.img`
+  @media (min-width: 1200px) {
+    &.landscape {
+      display: none;
+    }
+  }
+
+  @media (min-width: 400px) and (max-width: 1199px) {
+    &.portrait {
+      display: none;
+    }
+
+    &.landscape {
+      width: 105%;
+    }
+  }
+`

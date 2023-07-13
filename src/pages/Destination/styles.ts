94f2fdd0929cdc69c6d361547d86b7efe25004ce
Pedro Henrique <pedrohenriquefrancadedeus@gmail.com>
2023-07-13 13:52:04 -0300

    fix

--- src/pages/Destination/styles.ts
+++ src/pages/Destination/styles.ts
@@ -22,7 +22,6 @@ export const DestinationContainer = styled.div`
 
   @media (max-width: 640px) {
     background: url(${bgMobile}) no-repeat center center/cover;
-    overscroll-behavior-y: contain;
   }
 `
 
@@ -40,6 +39,9 @@ export const DestinationContent = styled.div`
     text-align: center;
 
     padding: 5.5rem 1.5rem 0;
+
+    overflow-y: scroll;
+    overscroll-behavior-y: contain;
   }
 `
 

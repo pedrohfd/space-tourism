30557feaec1c1eada310977097758d9737830583
Pedro Henrique <pedrohenriquefrancadedeus@gmail.com>
2023-07-14 11:25:14 -0300

    feat: add responsiveness to technology page

--- src/pages/Technology/styles.ts
+++ src/pages/Technology/styles.ts
@@ -38,6 +38,12 @@ export const TechnologyContent = styled.div`
   @media (min-width: 641px) and (max-width: 1007px) {
     flex-direction: column;
   }
+
+  @media (max-width: 640px) {
+    text-align: center;
+
+    padding-top: 5.5rem;
+  }
 `
 
 export const LeftContent = styled.div`
@@ -84,6 +90,21 @@ export const Informative = styled.h5`
       margin-right: 1.1rem;
     }
   }
+
+  @media (max-width: 640px) {
+    color: ${colors.white};
+
+    font-size: 1rem;
+    letter-spacing: 0.16rem;
+
+    margin-bottom: 2rem;
+
+    strong {
+      opacity: 0.25;
+      font-weight: 700;
+      margin-right: 1rem;
+    }
+  }
 `
 
 export const TechnologyButtonArea = styled.div`
@@ -95,6 +116,10 @@ export const TechnologyButtonArea = styled.div`
 
   @media (min-width: 641px) and (max-width: 1007px) {
     margin: 2.75rem 0;
+  }
+
+  @media (max-width: 640px) {
+    margin: 2rem 0 1.5rem 0;
   }
 `
 
@@ -141,6 +166,23 @@ export const TechnologyButton = styled.button<TechnologyButtonProps>`
       margin-left: 1rem;
     }
   }
+
+  @media (max-width: 640px) {
+    background: ${(props) => (props.isActive ? colors.white : 'transparent')};
+    border: 1px solid ${colors.white}40;
+    border-radius: 50%;
+    width: 2.5rem;
+    height: 2.5rem;
+    color: ${(props) => (props.isActive ? colors.black : colors.white)};
+    font-family: 'Bellefair', serif;
+    font-size: 1rem;
+
+    transition: all 0.2s;
+
+    & + button {
+      margin-left: 1rem;
+    }
+  }
 `
 
 export const Title = styled.h1`
@@ -158,6 +200,13 @@ export const Title = styled.h1`
     text-transform: uppercase;
     margin: 1rem 0;
   }
+
+  @media (max-width: 640px) {
+    color: ${colors.white};
+    font-size: 1.5rem;
+    text-transform: uppercase;
+    margin: 0.5rem 0 1rem 0;
+  }
 `
 
 export const Subtitle = styled.h5`
@@ -172,6 +221,12 @@ export const Subtitle = styled.h5`
     color: ${colors.blue};
     font-size: 1rem;
     letter-spacing: 0.16875rem;
+  }
+
+  @media (max-width: 640px) {
+    color: ${colors.blue};
+    font-size: 0.875rem;
+    letter-spacing: 0.14763rem;
   }
 `
 
@@ -191,6 +246,13 @@ export const Description = styled.p`
     color: ${colors.blue};
     line-height: 1.75rem;
   }
+
+  @media (max-width: 640px) {
+    color: ${colors.blue};
+    font-size: 0.9375rem;
+    line-height: 1.5625rem;
+    margin: 0 1.5rem;
+  }
 `
 
 export const TechnologyImage = styled.img`
@@ -201,6 +263,16 @@ export const TechnologyImage = styled.img`
   }
 
   @media (min-width: 641px) and (max-width: 1007px) {
+    &.portrait {
+      display: none;
+    }
+
+    &.landscape {
+      width: 105%;
+    }
+  }
+
+  @media (max-width: 640px) {
     &.portrait {
       display: none;
     }
